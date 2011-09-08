b00c57ca01e1d8fc0e7d212b5d06d96cb91be16f
mjpt777@gmail.com <mjpt777@gmail.com@bd14b42f-f725-63f0-54b1-68fa86419f67>
2011-09-08 09:11:11 +0000

    Make a little more terse

--- trunk/let-the-caller-choose/LetTheCallerChoose.java
+++ trunk/let-the-caller-choose/LetTheCallerChoose.java
@@ -1,13 +1,12 @@
 public final class LetTheCallerChoose
 {
-    public static int countDistinctWords1(final BufferedReader in)
+    public static int countDistinctWords1(BufferedReader in)
     {
         final Set<String> distinctWords = new HashSet<String>();
 
         String line = null;
         while (null != (line = in.readLine()))
         {
-            // Note: regex should be precompiled for performance
             final String[] words = line.split("+\\W"); 
             List<String> wordsAsList = Arrays.asList(words);
             distinctWords.addAll(wordsAsList);
@@ -16,14 +15,13 @@ public final class LetTheCallerChoose
         return distinctWords.size();
     }
 
-    public static int countDistinctWords2(final BufferedReader in)
+    public static int countDistinctWords2(BufferedReader in)
     {
         final Set<String> distinctWords = new HashSet<String>();
 
         String line = null;
         while (null != (line = in.readLine()))
         {
-            // Note: regex should be precompiled for performance
             line.split(distinctWords, "+\\W"); 
         }
 
